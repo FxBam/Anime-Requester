ced5c27967bb52f386e0b4eaceaa7bb3dbf1ae51
Ewen_Babin <babin241@unicaen.fr>
2025-09-29 10:34:10 +0200

    ajout de la clé d'api et d'un peu de code

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'a26406f42cmsh9bf2df33d395916p1a0ed6jsnae18570af90f',
+		'x-rapidapi-host': 'anime-db.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
